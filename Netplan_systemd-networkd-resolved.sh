bd18a793613bb6fffee472ea708d0eaa3b76e0b2
M.Nabi <58561138+nabi69@users.noreply.github.com>
2026-09-07 19:54:00 +0200

    Modify network settings for ens32 and ens33
    Updated network configuration for ens32 and ens33 with new IP addresses and routes.

--- Netplan_systemd-networkd-resolved.sh
+++ Netplan_systemd-networkd-resolved.sh
@@ -55,15 +55,34 @@ network:
   version: 2
   renderer: networkd
   ethernets:
-    ens35:
+    ens32:
       dhcp4: false
       addresses:
-        - 192.168.66.54/24
-      gateway4: 192.168.66.1
+        - 192.168.100.54/24
+      routes:
+        - to: default
+          via: 192.168.100.1
+          metric: 10
       nameservers:
         addresses:
           - 8.8.8.8
           - 1.1.1.1
+    ens33:
+      dhcp4: false
+      addresses:
+        - 192.168.1.54/24
+      routes:
+        - to: default
+          via: 192.168.1.1
+          metric: 100
+      nameservers:
+        addresses:
+          - 8.8.8.8
+          - 1.1.1.1
+    ens34:
+      dhcp4: false
+      addresses:
+        - 192.168.66.54/24
 ```
 
 > Replace `192.168.66.1` with your actual gateway if different.
